41b54636b9620b3085e39f8b33d040851405a017
SAURABH12047 <75216925+SAURABH12047@users.noreply.github.com>
2020-12-02 22:12:47 +0530

    initial changes

--- contact.php
+++ contact.php
@@ -2,22 +2,22 @@
 $name = $_POST['name'];
 $vistor_email = $_POST['email']
 $Query = $_POST['query'];
-$profession = $_POST['profession'];
-$city = $_POST['city'];
-$state = $_POST['state'];
-$other = $_POST['other'];
+// $profession = $_POST['profession'];
+// $city = $_POST['city'];
+// $state = $_POST['state'];
+// $other = $_POST['other'];
 $message = $_POST['tell_us_about_yourself'];
 $message2= $_POST['elaborate_your_cencern'];
 
-$email_form = 'Thisisvereassy@gail.com';
+$email_form = 'Whyuniverseisexist@gail.com';
 $email_subject = 'form_submission';
 $email_body = "User Name: $name.\n".
                 "User Email Address: $visitor_email.\n".
                 "User Query: $query.\n".
-                "User Profession: $profession.\n".
-                "User City: $City.\n".
-                "User State: $state.\n".
-                "User Other State: $other.\n".
+                // "User Profession: $profession.\n".
+                // "User City: $City.\n".
+                // "User State: $state.\n".
+                // "User Other State: $other.\n".
                 "User Message: $tell_us_about_yourself.\n".
                 "User Message2: $elaborate_your_cencern.\n";
 
