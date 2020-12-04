1dfce6e6a577223fb8c153346ffd88a4ea82b9fb
SAURABH12047 <75216925+SAURABH12047@users.noreply.github.com>
2020-12-04 21:29:48 +0530

    initial changes

--- contact.php
+++ contact.php
@@ -1,32 +1,32 @@
 <?php
-$name = $_POST['name'];
-$vistor_email = $_POST['email']
-$Query = $_POST['query'];
+$username = $_POST['username'];
+$email = $_POST['email']
+$mmessage = $_POST['message'];
 // $profession = $_POST['profession'];
 // $city = $_POST['city'];
 // $state = $_POST['state'];
 // $other = $_POST['other'];
-$message = $_POST['tell_us_about_yourself'];
-$message2= $_POST['elaborate_your_cencern'];
+// $message = $_POST['tell_us_about_yourself'];
+// $message2= $_POST['elaborate_your_cencern'];
 
 $email_form = 'Whyuniverseisexist@gail.com';
 $email_subject = 'form_submission';
-$email_body = "User Name: $name.\n".
-                "User Email Address: $visitor_email.\n".
-                "User Query: $query.\n".
+$email_body = "User Name: $username.\n".
+                "User Email: $email.\n".
+                "User message: $message.\n".
                 // "User Profession: $profession.\n".
                 // "User City: $City.\n".
                 // "User State: $state.\n".
                 // "User Other State: $other.\n".
-                "User Message: $tell_us_about_yourself.\n".
-                "User Message2: $elaborate_your_cencern.\n";
+//                 "User Message: $tell_us_about_yourself.\n".
+//                 "User Message2: $elaborate_your_cencern.\n";
 
 
                 $to = 'astrophysicist07070@gmail.com';
                 $headder = "form: $email_form\r\n";
                 $headder = "Replay-to: $vistor_email \r\n";
                 mail($to,$email_subject,$email_body,$headders);
-                headder("Location: index.html")
+                headder("Location: contact.html")
 
 
 
